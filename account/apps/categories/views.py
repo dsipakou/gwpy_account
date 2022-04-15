fb13fab3042e85824723f5515b0436af0d5202bc
Denis Sipakov <mc.flyer@gmail.com>
2022-04-15 11:30:09 +0200

    Different validations for category creating

--- account/apps/categories/views.py
+++ account/apps/categories/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from categories.serializers import CategorySerializer
 from categories.models import Category
@@ -8,8 +8,56 @@ class CategoryList(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    def perform_create(self, serializer):
+        """Check if parent category is not a child of any other category
+
+        Raises:
+            ValidationError: if parent category is already a child category
+        """
+
+        if (
+            serializer.validated_data["parent"]
+            and serializer.validated_data["parent"].parent is not None
+        ):
+            raise ValidationError("Child category cannot by parent category")
+        super().perform_create(serializer)
+
 
 class CategoryDetails(RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     lookup_field = "uuid"
+
+    def perform_update(self, serializer):
+        """Two checks:
+        - Child category cannot be parent
+        - Instance with childs cannot become child category
+
+        Raises:
+            ValidationError: if parent category is already a child category
+            ValidationError: when instance with at least one child converted to child category
+        """
+
+        if (
+            serializer.validated_data["parent"]
+            and serializer.validated_data["parent"].parent is not None
+        ):
+            raise ValidationError("Child category cannot by parent category")
+        if serializer.validated_data["parent"] is not None:
+            if Category.objects.filter(parent=serializer.instance).exists():
+                raise ValidationError(
+                    "This category has childs. Cannot convert to child category"
+                )
+
+        super().perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        """Check if instance has childs
+
+        Raises:
+            ValidationError: When instance has childs
+        """
+
+        if Category.objects.filter(parent=instance).exists():
+            raise ValidationError("Cannot delete non empty parent category")
+        super().perform_destroy(instance)

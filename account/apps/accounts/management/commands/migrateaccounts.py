3c90f986ac57897a15bbfeec47a441fab80d0fd2
Denis Sipakov <mc.flyer@gmail.com>
2022-04-16 20:49:45 +0200

    Accounts migrate command

---
+++ account/apps/accounts/management/commands/migrateaccounts.py
@@ -0,0 +1,38 @@
+import csv
+import uuid
+
+from accounts.models import Account
+from django.core.management.base import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("file_path", type=str)
+        parser.add_argument("first_user", type=str)
+        parser.add_argument("second_user", type=str)
+
+    def handle(self, *args, **kwars):
+        file_path = kwars["file_path"]
+
+        user_map = {
+            "1": kwars["first_user"],
+            "2": kwars["second_user"],
+        }
+
+        Account.objects.all().delete()
+
+        with open(file_path) as file:
+            data = list(csv.reader(file, delimiter=","))
+            print(len(data))
+            for row in data[1:]:
+                Account.objects.create(
+                    uuid=uuid.uuid4(),
+                    user=User.objects.get(uuid=user_map[row[1]]),
+                    source=row[2],
+                    amount=float(row[3]),
+                    description=row[4],
+                    is_main=False if row[7] == "false" else True,
+                    created_at=row[5],
+                    modified_at=row[6],
+                )
--- account/apps/accounts/models.py
+++ account/apps/accounts/models.py
@@ -7,7 +7,7 @@ class Account(models.Model):
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, to_field="uuid")
     source = models.CharField(max_length=30)
-    amount = models.IntegerField()
+    amount = models.FloatField()
     description = models.CharField(max_length=255, blank=True)
     is_main = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)

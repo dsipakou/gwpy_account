2e6b7e779c97f91b9975ca0d285df9d6eb886bb3
Denis Sipakov <mc.flyer@gmail.com>
2022-07-30 12:46:26 +0200

    Add category type

--- account/apps/categories/models.py
+++ account/apps/categories/models.py
@@ -5,6 +5,10 @@ from django.db.models import Q
 
 
 class Category(models.Model):
+    class Types(models.TextChoices):
+        INCOME = "INC", "Income"
+        EXPENSE = "EXP", "Expense"
+
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128)
     parent = models.ForeignKey(
@@ -15,6 +19,7 @@ class Category(models.Model):
         on_delete=models.CASCADE,
         to_field="uuid",
     )
+    type = models.CharField(max_length=3, choices=Types.choices, default=Types.EXPENSE)
     is_income = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
